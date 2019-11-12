30a71efa1dacf7c1c246dfe03a726bc0cbddaf19
rkhanna36 <rkhanna36@dxc.com>
2019-11-12 16:46:26 +0530

    master

---
+++ src/main/java/com/example/music/MusicStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApplication.class, args);
+	}
+
+}
